072325cdbb3aa9f25532ac35ef44cdff8af5a3d1
Kaio Soledade <soledadekaio@gmail.com>
2023-04-15 14:58:32 -0300

    adição de skrollreveal no h1 de cada seção

---
+++ script.js
@@ -0,0 +1,12 @@
+
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal ('.h1-texto1', {durantion: 2000});
+
+sr.reveal ('.h1-texto2', {
+    rotate: {x: 0, y: 80, z: 0},
+    durantion: 2000});
+
+sr.reveal ('.h1-texto3', 
+    rotate: {x: 100, y: 0, z: 0},
+    {durantion: 2000});
